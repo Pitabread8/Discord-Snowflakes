4b609acdd5cef4de532eff891b8ed9eee1d39f56
Sita <sitavemuri8@gmail.com>
2020-12-23 15:07:08 -0800

    Update files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Discord-Snowflakes
+
+Discord uses Snowflake to generate IDs for accounts, messages, channels, and servers. This website extracts the timestamps from the IDs. You can easily find out how long someone has been on Discord, when a channel/server was created, or when a message was sent.
+
+In order to copy an ID, make sure you have enabled Developer Mode in User Settings > Appearance. Then, right click on the element and choose the Copy ID option.
+
+Inspired by: https://pixelatomy.com/snow-stamp/
+Discord Snowflake info: https://discord.com/developers/docs/reference#convert-snowflake-to-datetime
--- snowflake.js
+++ snowflake.js
@@ -1,42 +1,24 @@
 let input = document.getElementById("input");
-let submit = document.getElementById("submit");
+let reset = document.getElementById("reset");
 let result = document.getElementById("result");
-let options = document.getElementsByClassName("options");
 
-for (i = 0; i < options.length; i++) {options[i].style.display = "none";}
+result.style.display = "none";
 
 input.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-        submit.click();
-        input.maxlength = 0;
+    if (event.keyCode === 82) {
+        reset.click();
+    }
+    else {
+        convertNumber(input.value);
     }
 });
 
-submit.addEventListener("click", () => {
-    chooseOption();
-    result.style.display = "none";
+reset.addEventListener("click", () => {
+    input.value = '';
+    result.innerHTML = '';
 });
 
-let user = document.getElementById("user");
-let msg = document.getElementById("msg");
-let chnl = document.getElementById("chnl");
-let srvr = document.getElementById("srvr");
-
-input.addEventListener("keyup", function(event) {if (event.keyCode === 49) {user.click();}});
-input.addEventListener("keyup", function(event) {if (event.keyCode === 50) {msg.click();}});
-input.addEventListener("keyup", function(event) {if (event.keyCode === 51) {chnl.click();}});
-input.addEventListener("keyup", function(event) {if (event.keyCode === 52) {srvr.click();}});
-
-function chooseOption() {
-    for (i = 0; i < options.length; i++) {options[i].style.display = "initial";}
-    user.addEventListener("click", function() {convertNumber(input.value, "The account was created on:");});
-    msg.addEventListener("click", function() {convertNumber(input.value, "The message was sent on:");});
-    chnl.addEventListener("click", function() {convertNumber(input.value, "The channel was created on:");});
-    srvr.addEventListener("click", function() {convertNumber(input.value, "The server was created on:");});
-}
-
-function convertNumber(number, option) {
-    for (i = 0; i < options.length; i++) {options[i].style.display = "none";}
+function convertNumber(number) {
     let id = parseInt(number);
 
     let binary = id.toString(2);
@@ -50,8 +32,7 @@ function convertNumber(number, option) {
 
     const date = new Date(unix);
     const dataFormat = date.toLocaleString();
-    
-    // console.log(option, dataFormat);
+
     result.style.display = "initial";
-    result.innerHTML= `${option} ${dataFormat}`;
+    result.innerHTML= `${dataFormat}`;
 }
